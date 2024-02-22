873d2c477499e33c2955e82383e06d7f580b882e
LarZ <129504789+LarZ@users.noreply.github.com>
2024-02-22 17:09:27 +0100

    Update tastetrykk_reg.py
    translating to english

--- tastetrykk_reg.py
+++ tastetrykk_reg.py
@@ -1,47 +1,47 @@
-# registrere tastetrykk og lagre med tekstfil
+# registering keystrokes, saving the count to a textfile txt
 
 import time
 import keyboard
 
-path = 'C:\\Python\\tekstfiler\\tastetrykk.txt'
+path = 'C:\\Python\\keystrokes.txt'
 
-tastetrykk = 0
+keystrokes = 0
 
 def les_fil():
-    global tastetrykk
+    global keystrokes
     with open(path, 'r') as fil:
         tastetrykk_lest = fil.read()
-    tastetrykk = int(tastetrykk_lest)
+    keystrokes = int(keystrokes_read)
     fil.close()
-    return tastetrykk
+    return keystrokes
 
 
-def tastetrykk_reg(e):
-    global tastetrykk
-    tastetrykk += 1
+def keystrokes_reg(e):
+    global keystrokes
+    keystrokes += 1
     
     skriv_antall()
     
 
 def lagre():
-    global tastetrykk
+    global keystrokes
     with open(path, 'w') as fil:
-        fil.write(str(tastetrykk))
+        fil.write(str(keystrokes))
         fil.close()
         
     nå=time.ctime()
     print (f'lagret {nå}')
-    print (f'tastetrykk siden 22.02.2024: {tastetrykk}')
+    print (f'number of keystrokes: {keystrokes}')
     
         
 def skriv_antall():
-    global tastetrykk
-    print (tastetrykk)
+    global keystrokes
+    print (keystrokes)
 
 
 les_fil()
     
-keyboard.on_press(tastetrykk_reg)
+keyboard.on_press(keystrokes_reg)
 
 while True:
     time.sleep(60)
